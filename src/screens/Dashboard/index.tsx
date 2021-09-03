55ef36fa0cb6d0b2603f32ce3317dde108c2d154
Lupércio <lupercio.ferraz@gmail.com>
2021-09-03 14:15:03 -0300

    Creating Theme, and Dashboard page

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { Container, Title } from './styles';
+import theme from '../../global/styles/theme'
+
+export function Dashboard() {
+
+    return(
+        
+        <Container>
+
+            <Title>Dashboard</Title>
+
+        </Container>
+        
+
+    );
+
+
+}
